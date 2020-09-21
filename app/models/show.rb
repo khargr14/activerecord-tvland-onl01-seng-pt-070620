00d7254c3b4d9222c231e049ce51427ca7e82b26
Keesha Hargrove <kharg14@gmail.com>
2020-09-21 23:26:04 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -2,11 +2,11 @@
   
 # end
 
-class Show < ActiveRecord::Base
+# class Show < ActiveRecord::Base
 
-    has_many :characters
-    belongs_to :network
-    def actors_list
-        ["#{Actor.all[0].first_name} #{Actor.all[0].last_name}"]
-    end
-end
+#     has_many :characters
+#     belongs_to :network
+#     def actors_list
+#         ["#{Actor.all[0].first_name} #{Actor.all[0].last_name}"]
+#     end
+# end
