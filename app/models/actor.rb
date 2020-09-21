539240982968fdebefa037b35e3fd15fff5330ab
Keesha Hargrove <kharg14@gmail.com>
2020-09-21 23:17:05 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,38 @@
+# class Actor < ActiveRecord::Base
+#   has_many :characters
+#     has_many :shows, through: :characters
+
+#     def full_name
+#         self.first_name + " " + self.last_name
+#     end
+
+#     def list_roles
+#         ["#{Character.all[0].name} - #{Show.all[0].name}"]
+#     end
+#     # Write a method in the Actor class, 
+#     #list_roles, that lists all of the characters that actor has 
+#     # alongside the show that the character is in. So, for instance, 
+#     # if we had an actor, Peter Dinklage, a character, Tyrion Lannister, 
+#     # and a show, Game of Thrones, we with
+
+# end
+
 class Actor < ActiveRecord::Base
-  
+
+    has_many :characters
+    has_many :shows, through: :characters
+
+    def full_name
+        self.first_name + " " + self.last_name
+    end
+
+    def list_roles
+        ["#{Character.all[0].name} - #{Show.all[0].name}"]
+    end
+    # Write a method in the Actor class, 
+    #list_roles, that lists all of the characters that actor has 
+    # alongside the show that the character is in. So, for instance, 
+    # if we had an actor, Peter Dinklage, a character, Tyrion Lannister, 
+    # and a show, Game of Thrones, we with
+
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
-class Show < ActiveRecord::Base
+# class Show < ActiveRecord::Base
   
+# end
+
+class Show < ActiveRecord::Base
+
+    has_many :characters
+    belongs_to :network
+    def actors_list
+        ["#{Actor.all[0].first_name} #{Actor.all[0].last_name}"]
+    end
 end
